d8e59a55e46fabdc2e05a5565fc60a120259c5a5
Pavlo Grigoriev <gpavlo2020@gmail.com>
2023-04-10 00:18:27 +0300

    task 08_2 solved
    task 2 solved

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
 
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const currentTimeKey="videoplayer-current-time";
+
+player.on("timeupdate",throttle(onPlayerTimeUpdate,1000));
+
+function onPlayerTimeUpdate(evt){
+    const currentTimeValue=JSON.stringify(evt.seconds);
+    localStorage.setItem(currentTimeKey,currentTimeValue);
+};
+
+const timeToContinue=JSON.parse(localStorage.getItem(currentTimeKey));
+player.setCurrentTime(timeToContinue||0);
