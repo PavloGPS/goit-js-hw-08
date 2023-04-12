305543d7a9383aaab5477b2cef053c7413cc4c85
Pavlo Grigoriev <gpavlo2020@gmail.com>
2023-04-13 01:46:20 +0300

    Update 02-video.js
    formates with Prettier

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,16 @@
-
-import Player from "@vimeo/player";
-import throttle from "lodash.throttle";
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-const currentTimeKey="videoplayer-current-time";
+const currentTimeKey = 'videoplayer-current-time';
 
-player.on("timeupdate",throttle(onPlayerTimeUpdate,1000));
+player.on('timeupdate', throttle(onPlayerTimeUpdate, 1000));
 
-function onPlayerTimeUpdate(evt){
-    const currentTimeValue=JSON.stringify(evt.seconds);
-    localStorage.setItem(currentTimeKey,currentTimeValue);
-};
+function onPlayerTimeUpdate(evt) {
+  const currentTimeValue = JSON.stringify(evt.seconds);
+  localStorage.setItem(currentTimeKey, currentTimeValue);
+}
 
-const timeToContinue=JSON.parse(localStorage.getItem(currentTimeKey));
-player.setCurrentTime(timeToContinue||0);
+const timeToContinue = JSON.parse(localStorage.getItem(currentTimeKey));
+player.setCurrentTime(timeToContinue || 0);
