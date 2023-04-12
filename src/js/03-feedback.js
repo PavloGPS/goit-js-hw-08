61830f66bcc5a8731af62a10e3ab93f6968b02e6
Pavlo Grigoriev <gpavlo2020@gmail.com>
2023-04-13 01:45:06 +0300

    Update 03-feedback.js
    task 08_3 solved

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  feedbackForm: document.querySelector('form.feedback-form'),
+  inputEmail: document.querySelector('input[name="email"]'),
+  inputMessage: document.querySelector('textarea[name="message"]'),
+};
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const formDataObj = {};
+
+fillFormData();
+
+// Event delegation:
+refs.feedbackForm.addEventListener(
+  'input',
+  throttle(evt => {
+    formDataObj.email = refs.inputEmail.value;
+    formDataObj.message = refs.inputMessage.value;
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formDataObj));
+  }, 500)
+);
+
+refs.feedbackForm.addEventListener('submit', onFeedbackFormSubmit);
+
+function onFeedbackFormSubmit(evt) {
+  evt.preventDefault();
+  // imitation of sending form
+  console.log('Sending data:', formDataObj);
+  evt.currentTarget.reset();
+  console.log(evt.currentTarget);
+
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+function fillFormData() {
+  const savedData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+  if (savedData) {
+    formDataObj.email = savedData.email;
+    formDataObj.message = savedData.message;
+    refs.inputEmail.value = formDataObj.email;
+    refs.inputMessage.value = formDataObj.message;
+  }
+}
